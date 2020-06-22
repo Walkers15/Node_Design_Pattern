d697ab4150d1b59c0a0d3c5868ab0c831f948a3d
백승훈 <46189249+Walkers15@users.noreply.github.com>
2020-06-22 15:06:10 +0900

    Add files via upload

---
+++ webpack_es2015/src/sayHello.js
@@ -0,0 +1,7 @@
+const mustache = require('mustache')
+const template = '<h1>Hello<i>{{name}}</i></h1>';
+
+mustache.parse(template);
+module.exports.sayHello = toWhom => {
+    return mustache.render(template, {name:toWhom});
+};
